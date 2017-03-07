36025531e304f7527f15aacd16440ff7392abb21
elen elen <elen elen>
2017-03-07 08:59:42 +0200

    ready controller

--- index.php
+++ index.php
@@ -6,7 +6,12 @@
 require('config.php');
 
 require 'classes/Bootstrap.php';
+require 'classes/Controller.php';
 
 $bootstrap = new Bootstrap($_GET);
 
 $controller = $bootstrap->createController();
+
+if($controller){
+	$controller->execute_action();
+}

1353b63aa7e2294046564f2e02905e977aa26de9
elen elen <elen elen>
2017-04-05 17:38:33 +0300

    add home controller

--- index.php
+++ index.php
@@ -7,11 +7,12 @@ require('config.php');
 
 require 'classes/Bootstrap.php';
 require 'classes/Controller.php';
+require 'controllers/Home.php';
 
 $bootstrap = new Bootstrap($_GET);
 
 $controller = $bootstrap->createController();
 
 if($controller){
-	$controller->execute_action();
+	$controller->execute_action();	
 }
